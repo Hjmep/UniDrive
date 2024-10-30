0f57ef6a67b2979dba6dddc8bce8e1f4dc77260e
Hjmep <gbessouvegnijaimepacmerithound@gmail.com>
2024-10-30 04:58:22 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -1,7 +1,7 @@
 // config.js
 export const config = {
   web: {
-    api_key: "Paste API Key Here",
-    client_id: "Paste Client ID Here",
+    api_key: "https://924207783507-8c16iub9hq7bsv17ekp4m9hpoiiufgst.apps.googleusercontent.com",
+    client_id: "https://924207783507-u7v3nkmi0crad09q746250d3s3l1u4sq.apps.googleusercontent.com",
   }
 };
