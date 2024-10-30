f6790fdfd667e2c66951f3e1dc92e7ea0df8e105
Hjmep <gbessouvegnijaimepacmerithound@gmail.com>
2024-10-30 07:17:54 +0100

    Update config.js

--- src/config.js
+++ src/config.js
@@ -2,6 +2,7 @@
 export const config = {
   web: {
     api_key: "AIzaSyDWbG_msZFzfOZ9qClaNw7e1NobUdlQp1k",
-    client_id: "https://924207783507-u7v3nkmi0crad09q746250d3s3l1u4sq.apps.googleusercontent.com",
+    client_id: "924207783507-u7v3nkmi0crad09q746250d3s3l1u4sq.apps.googleusercontent.com", // Assure-toi que l'ID est correct
+    client_secret: "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" // Remplace ceci par ton secret client
   }
 };
